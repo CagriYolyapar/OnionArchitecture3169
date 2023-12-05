a0a14f26918e7debc8237defe39795ca178eabb6
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-12-05 13:53:45 +0300

    Add project files.

---
+++ Infrastructure/Project.Persistence/ContextClasses/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Persistence.ContextClasses
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
